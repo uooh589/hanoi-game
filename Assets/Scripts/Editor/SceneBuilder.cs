67f47574222b59a25f5d9e672d639b2d4c48eb0e
uooh589 <uooh589@github.com>
2026-05-18 23:49:35 +0800

    fix: avatar anchored to left edge, brighter element card backgrounds

--- Assets/Scripts/Editor/SceneBuilder.cs
+++ Assets/Scripts/Editor/SceneBuilder.cs
@@ -252,7 +252,17 @@ namespace HanoiGame
             piBg.raycastTarget = false;
 
             // Traveler avatar
-            var pAvatar = NewImage(pi.transform, "PlayerAvatar", Color.clear, new Vector2(60, 60), new Vector2(50, -55));
+            // Avatar at left edge of PlayerInfo panel (panel 300x110, top-left pivot)
+            var pAvatar = new GameObject("PlayerAvatar", typeof(Image));
+            pAvatar.transform.SetParent(pi.transform, false);
+            var paImg = pAvatar.GetComponent<Image>();
+            paImg.color = Color.white; paImg.raycastTarget = true;
+            var paRt = pAvatar.GetComponent<RectTransform>();
+            paRt.anchorMin = paRt.anchorMax = new Vector2(0, 0.5f);
+            paRt.pivot = new Vector2(0, 0.5f);
+            paRt.sizeDelta = new Vector2(56, 56);
+            paRt.anchoredPosition = new Vector2(8, 0);
+            pAvatar.AddComponent<Button>();
             pAvatar.type = Image.Type.Simple; pAvatar.preserveAspect = true; pAvatar.raycastTarget = true;
             pAvatar.gameObject.AddComponent<Button>();
 
